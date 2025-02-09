1a59632cd87abbb0ba13b377c4638c7856daaba2
sanket005 <110851111+sanket005@users.noreply.github.com>
2025-02-09 01:33:54 -0800

    Update Hooks.java

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -30,11 +30,11 @@ public void setup() {
 }
 
 @After 
-public void tearDown(Scenario sc) throws IOException {
-	String scname=sc.getName().replace(" ", "_");
-	if(sc.failed()){
+public void tearDown(Scenario scn) throws IOException {
+	String scname=scn.getName().replace(" ", "_");
+	if(scn.failed()){
 		TakesScreenshot Ts=(TakesScreenshot) driver;
-		sc.attach(Ts.getScreenshotAs(OutputType.BYTES),"image/png",scname);
+		scn.attach(Ts.getScreenshotAs(OutputType.BYTES),"image/png",scname);
 	}
 	driver.close();
 	File file=new File("D:\\CucumberFramework\\target\\Report.html");
